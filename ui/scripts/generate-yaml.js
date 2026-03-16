0bf383ea66095dadd5e026b15e451e7652e7a298
Sushmita15 <sushmitamandal100@gmail.com>
2026-03-16 17:49:50 -0400

    changed discuit to WhyIluv

--- ui/scripts/generate-yaml.js
+++ ui/scripts/generate-yaml.js
@@ -3,8 +3,11 @@ import fs from 'fs';
 import path from 'path';
 
 // Get the root path
-const rootPath = path.join(import.meta.dirname, '../../');
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const rootPath = path.join(__dirname, '../../');
 // Run the command to get the config
 (async () => {
   await exec('./discuit inject-config', { cwd: '../' }, async (error, stdout, stderr) => {
